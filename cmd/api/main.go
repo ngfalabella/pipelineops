c6400db386b2cfec6568b095f65ecf36cca84dd9
Nicolas <nicolasgfalabella@gmail.com>
2026-08-22 14:46:42 -0300

    feat: add health check endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,11 +10,21 @@ func handlerInit(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Inicio de aplicacion")
 }
 
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "OK")
+	default:
+		http.Error(w, "Metodo no disponible", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	const port int = 8080
 	var addr string = fmt.Sprintf(":%d", port)
 
 	http.HandleFunc("/", handlerInit)
+	http.HandleFunc("/healthz", handlerHealthz)
 
 	log.Println("Servidor corriendo en puerto : ", port)
 
