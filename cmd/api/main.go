24ec19668b0abc3e80c29bf574fd3d7f856bcee8
Nicolas <nicolasgfalabella@gmail.com>
2026-08-22 14:12:08 -0300

    feat: add basic HTTP server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handlerInit(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Inicio de aplicacion")
+}
+
 func main() {
+	const port int = 8080
+	var addr string = fmt.Sprintf(":%d", port)
+
+	http.HandleFunc("/", handlerInit)
+
+	log.Println("Servidor corriendo en puerto : ", port)
+
+	err := http.ListenAndServe(addr, nil)
+
+	if err != nil {
+		log.Fatalln("Ocurrio un error al iniciar servidor", err)
+	}
 
 }
